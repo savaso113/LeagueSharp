d48ea590f3ef346acccf797689608d09e70d27f6
TheNinow <V8II@gmx.at>
2015-05-07 18:50:24 +0200

    TheCheater

--- Private/TheCheater/TheCheater/Properties/AssemblyInfo.cs
+++ Private/TheCheater/TheCheater/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TheCheater")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Private assembly, NOT allowed on joduska.me!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TheCheater")]
