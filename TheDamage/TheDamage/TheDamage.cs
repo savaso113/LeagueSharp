bd749cb2ae3d522e6a3ff7ef0a2b004eceee90fd
TheNinow <V8II@gmx.at>
2015-08-01 16:53:02 +0200

    thedamage 1.1

--- TheDamage/TheDamage/TheDamage.cs
+++ TheDamage/TheDamage/TheDamage.cs
@@ -58,17 +58,33 @@ namespace TheDamage
                     Text[slot] = new Render.Text(string.Empty, Vector2.Zero, 16, new ColorBGRA(0)) { Visible = false };
                     Text[slot].Add();
                 }
+                Text[SpellSlot.Unknown] = new Render.Text(string.Empty, Vector2.Zero, 16, new ColorBGRA(0)) { Visible = false };
+                Text[SpellSlot.Unknown].Add();
 
-                _menu.AddItem(new MenuItem(_menu.Name + ".dontdrawoncd", "Don't draw when on cooldown").SetValue(true));
-                _menu.AddItem(new MenuItem(_menu.Name + ".DrawAsOneOnClutter", "Draw only one bar when small").SetValue(true));
-                _menu.AddItem(new MenuItem(_menu.Name + ".GeneralColor", "General Color").SetValue(Color.FromArgb(150, Color.OrangeRed)));
+                var miscMenu = new Menu("Misc", _menu.Name + ".Misc");
+                miscMenu.AddItem(new MenuItem(_menu.Name + ".dontdrawoncd", "Don't draw when on cooldown").SetValue(true));
+                miscMenu.AddItem(new MenuItem(_menu.Name + ".DrawAsOneOnClutter", "Draw only one bar when small").SetValue(true));
+                miscMenu.AddItem(new MenuItem(_menu.Name + ".GeneralColor", "General Color").SetValue(Color.FromArgb(150, Color.OrangeRed)));
+                var upvoted = miscMenu.AddItem(new MenuItem(_menu.Name + ".upvotedasmdb", "Upvoted on assemblydb").SetValue(false));
+                _menu.AddSubMenu(miscMenu);
+
                 _menu.AddItem(new MenuItem(_menu.Name + ".Enabled", "Enabled").SetValue(true));
                 _menu.AddToMainMenu();
 
                 _permashow = new MenuItem(_menu.Name + ".Target", "The Damage").SetValue(new StringList(new[] { "None" }));
                 _permashow.Permashow();
 
+
+                upvoted.ValueChanged += (sender, changedArgs) =>
+                {
+                    if (changedArgs.GetNewValue<bool>())
+                        Notifications.AddNotification("Thank you! :)", 3);
+                };
+                if (upvoted.GetValue<bool>())
+                    miscMenu.Items.Remove(upvoted);
+
                 Drawing.OnDraw += Draw;
+
             };
         }
 
@@ -95,6 +111,17 @@ namespace TheDamage
             var prevPlayerHealthPercent = playerHealthPercent;
             var hasDrawn = false;
 
+            if (GetRemainingIgniteDamage(ObjectManager.Player) > ObjectManager.Player.Health + ObjectManager.Player.HPRegenRate * 2.5f)
+            {
+                var fullHealthPercent = 1d;
+                var drawLines = false;
+                foreach (var text in Text)
+                    if (text.Key != SpellSlot.Unknown)
+                        text.Value.Visible = false;
+                DrawDamageOnHealthbar(ObjectManager.Player, ObjectManager.Player.MaxHealth, ref fullHealthPercent, 1, ref drawLines, Color.FromArgb(150, Color.Red), "IGNITE KILLS", true, SpellSlot.Unknown, false);
+                return;
+            }
+
             if (_menu.Item(_menu.Name + ".DrawAsOneOnClutter").GetValue<bool>())
             {
                 var sumdmg = SupportedSlots.Select(slot => target.GetSpellDamage(ObjectManager.Player, slot)).Sum();
@@ -108,9 +135,8 @@ namespace TheDamage
 
             }
 
-            for (int index = 0; index < SupportedSlots.Length; index++)
+            foreach (var spellSlot in SupportedSlots)
             {
-                var spellSlot = SupportedSlots[index];
                 if (target.GetSpell(spellSlot).Level == 0 || (target.GetSpell(spellSlot).CooldownExpires > Game.Time && _menu.Item(_menu.Name + ".dontdrawoncd").GetValue<bool>()) || !_enemiesMenu.Item(_enemiesMenu.Name + "." + target.ChampionName + "." + spellSlot).GetValue<bool>())
                 {
                     Text[spellSlot].Visible = false;
@@ -121,28 +147,6 @@ namespace TheDamage
                 var damage = target.GetSpellDamage(ObjectManager.Player, spellSlot);
 
                 DrawDamageOnHealthbar(ObjectManager.Player, damage, ref playerHealthPercent, prevPlayerHealthPercent, ref hasDrawn, spellColor, true, spellSlot);
-                //var position = ObjectManager.Player.HPBarPosition + Offset;
-
-
-                //playerHealthPercent = Math.Max(playerHealthPercent - (damage) / ObjectManager.Player.MaxHealth, 0);
-
-                //var start = new Vector2((float)(position.X + playerHealthPercent * Width), position.Y);
-                //var end = new Vector2((float)((prevPlayerHealthPercent) * Width) + position.X, position.Y);
-
-
-
-                //if (end.X - start.X > 1)
-                //{
-                //    Drawing.DrawLine(start, end, Height, spellColor);
-                //    if (hasDrawn)
-                //    {
-                //        Drawing.DrawLine(end, new Vector2(end.X, end.Y + Height), 2, Color.Black);
-                //    }
-                //    hasDrawn = true;
-                //}
-
-
-
 
                 prevPlayerHealthPercent = playerHealthPercent;
             }
@@ -156,7 +160,13 @@ namespace TheDamage
             }
         }
 
-        private static void DrawDamageOnHealthbar(Obj_AI_Hero healthbar, double damage, ref double playerHealthPercent, double prevPlayerHealthPercent, ref bool shouldDrawLine, Color spellColor, bool drawslot = false, SpellSlot slot = SpellSlot.Unknown)
+        private static void DrawDamageOnHealthbar(Obj_AI_Hero healthbar, double damage, ref double playerHealthPercent, double prevPlayerHealthPercent, ref bool shouldDrawLine, Color spellColor, bool drawtext = false, SpellSlot slot = SpellSlot.Unknown)
+        {
+            DrawDamageOnHealthbar(healthbar, damage, ref playerHealthPercent, prevPlayerHealthPercent, ref shouldDrawLine, spellColor, slot.ToString(), drawtext, slot);
+        }
+
+
+        private static void DrawDamageOnHealthbar(Obj_AI_Hero healthbar, double damage, ref double playerHealthPercent, double prevPlayerHealthPercent, ref bool shouldDrawLine, Color spellColor, string text, bool drawtext = false, SpellSlot slot = SpellSlot.Unknown, bool colorDown = true)
         {
             var position = healthbar.HPBarPosition + Offset;
             playerHealthPercent = Math.Max(playerHealthPercent - (damage) / ObjectManager.Player.MaxHealth, 0);
@@ -173,15 +183,30 @@ namespace TheDamage
                 shouldDrawLine = true;
             }
 
-            if (drawslot)
+            if (drawtext)
             {
-                Text[slot].Visible = (end.X - start.X >= Text[slot].Width);
-                Text[slot].text = slot.ToString();
+                Text[slot].text = text;
                 Text[slot].X = (int)(start.X + (end.X - start.X) / 2f - Text[slot].Width / 2f);
-                Text[slot].Y = (int)(start.Y - 3);
-                Text[slot].Color = new SharpDX.Color(spellColor.R - 75, spellColor.G - 75, spellColor.B - 75);
+                Text[slot].Y = (int)(start.Y - 3.25f);
+                if (colorDown)
+                    Text[slot].Color = new SharpDX.Color(spellColor.R - 75, spellColor.G - 75, spellColor.B - 75);
+                else
+                    Text[slot].Color = new SharpDX.Color(spellColor.R + 75, spellColor.G + 75, spellColor.B + 75);
+                Text[slot].Visible = (end.X - start.X >= Text[slot].Width);
             }
 
+        }
+
+        private static float GetIgniteDamage(Obj_AI_Hero souce)
+        {
+            return 50 + souce.Level * 20;
+        }
+
+        private static float GetRemainingIgniteDamage(Obj_AI_Base target)
+        {
+            var ignitebuff = target.GetBuff("summonerdot");
+            if (ignitebuff == null) return 0;
+            return (float)ObjectManager.Player.CalcDamage(target, Damage.DamageType.True, ((int)(ignitebuff.EndTime - Game.Time) + 1) * GetIgniteDamage(ignitebuff.Caster as Obj_AI_Hero) / 5);
         }
 
     }
